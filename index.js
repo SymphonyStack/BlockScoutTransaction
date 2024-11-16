634e119d2dc550f9ff716ec2ab1b00910a2a6609
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 13:34:10 +0530

    ft: init

---
+++ index.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const args = process.argv.slice(2);
+const address = [args[0]];
+
+const run = () => {
+  axios
+    .get(`https://eth.blockscout.com/api/v2/addresses/${address}`)
+    .then(res => {
+      console.log("##", res.data, "##");
+    });
+};
+run();
